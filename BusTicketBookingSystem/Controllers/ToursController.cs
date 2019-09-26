7aec886a7397689f231d8beabba0d15a79ae3508
Man Ioan <man_ioan@live.com>
2019-09-26 19:28:06 +0300

    Add Tours&Reservations Module

---
+++ BusTicketBookingSystem/Controllers/ToursController.cs
@@ -0,0 +1,146 @@
+﻿using BusTicketBookingSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BusTicketBookingSystem.Controllers
+{
+    public class ToursController : Controller
+    {
+        private TicketBookingModelEntities db = new TicketBookingModelEntities();
+
+        // GET: Tours
+        [Authorize]
+        public ActionResult Index()
+        {
+            var tours = db.Tours.Include(t => t.Bus).Include(t => t.City);
+            return View(tours.ToList());
+        }
+
+       
+
+        // GET: Tours/Details/5
+        [Authorize]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tour tour = db.Tours.Find(id);
+            if (tour == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tour);
+        }
+
+        // GET: Tours/Create
+        [Authorize(Roles = "Admin")]
+        public ActionResult Create()
+        {
+            ViewBag.BusId = new SelectList(db.Buses, "BusId", "NrReg");
+            ViewBag.CityId = new SelectList(db.Cities, "CityId", "Name");
+            return View();
+        }
+
+        // POST: Tours/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public ActionResult Create([Bind(Include = "TourId,TourDate,CityId,Departure,Arrival,BusId,SeatsAvailable,Price")] Tour tour)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tours.Add(tour);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.BusId = new SelectList(db.Buses, "BusId", "NrReg", tour.BusId);
+            ViewBag.CityId = new SelectList(db.Cities, "CityId", "Name", tour.CityId);
+            return View(tour);
+        }
+
+        // GET: Tours/Edit/5
+        [Authorize(Roles = "Admin")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tour tour = db.Tours.Find(id);
+            if (tour == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.BusId = new SelectList(db.Buses, "BusId", "NrReg", tour.BusId);
+            ViewBag.CityId = new SelectList(db.Cities, "CityId", "Name", tour.CityId);
+            return View(tour);
+        }
+
+        // POST: Tours/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public ActionResult Edit([Bind(Include = "TourId,TourDate,CityId,Departure,Arrival,BusId,SeatsAvailable,Price")] Tour tour)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tour).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.BusId = new SelectList(db.Buses, "BusId", "NrReg", tour.BusId);
+            ViewBag.CityId = new SelectList(db.Cities, "CityId", "Name", tour.CityId);
+            return View(tour);
+        }
+
+        // GET: Tours/Delete/5
+        [Authorize(Roles = "Admin")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tour tour = db.Tours.Find(id);
+            if (tour == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tour);
+        }
+
+        // POST: Tours/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tour tour = db.Tours.Find(id);
+            db.Tours.Remove(tour);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
