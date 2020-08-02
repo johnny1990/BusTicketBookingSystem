0d513b515e809e1b21723cb04ea690b03b4f147a
Man Ioan <man_ioan@live.com>
2020-08-02 17:17:41 +0300

    Add drivers Controller & views

---
+++ BusTicketBookingSystem/Controllers/DriversController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BusTicketBookingSystem.Entities.Models;
+using BusTicketBookingSystem.Repository.Interfaces;
+
+namespace BusTicketBookingSystem.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class DriversController : Controller
+    {
+        private readonly IDriversRepository repository;
+
+        public DriversController(IDriversRepository objIrepository)
+        {
+            repository = objIrepository;
+        }
+
+        private TicketBookingModelEntities db = new TicketBookingModelEntities();
+
+        // GET: Drivers
+        public ActionResult Index()
+        {
+            return View(repository.All.ToList());
+        }
+
+        // GET: Drivers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Driver driver = repository.Find(id);
+            if (driver == null)
+            {
+                return HttpNotFound();
+            }
+            return View(driver);
+        }
+
+        // GET: Drivers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Drivers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DriverId,Name,SerialNumber,DriverLicence,PhoneNumber,EmailAddress,ProfilePicture,IsAvailable")] Driver driver)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.Insert(driver);
+                repository.Save();
+                return RedirectToAction("Index");
+            }
+
+            return View(driver);
+        }
+
+        // GET: Drivers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Driver driver = repository.Find(id);
+            if (driver == null)
+            {
+                return HttpNotFound();
+            }
+            return View(driver);
+        }
+
+        // POST: Drivers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DriverId,Name,SerialNumber,DriverLicence,PhoneNumber,EmailAddress,ProfilePicture,IsAvailable")] Driver driver)
+        {
+            if (ModelState.IsValid)
+            {
+              //???  db.Entry(driver).State = EntityState.Modified;
+                repository.Save();
+                return RedirectToAction("Index");
+            }
+            return View(driver);
+        }
+
+        // GET: Drivers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Driver driver = repository.Find(id);
+            if (driver == null)
+            {
+                return HttpNotFound();
+            }
+            return View(driver);
+        }
+
+        // POST: Drivers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Driver driver = repository.Find(id);
+            repository.Delete(id);
+            repository.Save();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                repository.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
