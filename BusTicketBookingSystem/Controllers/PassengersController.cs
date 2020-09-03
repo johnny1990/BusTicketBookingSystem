ac46eabf840b8f152a7e7e7c010ff91f9561f401
Man Ioan <man_ioan@live.com>
2020-09-03 19:24:59 +0300

    Commit Add passengers module

---
+++ BusTicketBookingSystem/Controllers/PassengersController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BusTicketBookingSystem.Entities.Models;
+using BusTicketBookingSystem.Repository.Interfaces;
+
+namespace BusTicketBookingSystem.Controllers
+{
+    public class PassengersController : Controller
+    {
+
+        private readonly IPassengersRepository repository;
+
+        public PassengersController( IPassengersRepository objIrepository)
+        {
+            repository = objIrepository;
+        }
+
+        // GET: Passengers
+        public ActionResult Index()
+        {
+            return View(repository.All.ToList());
+        }
+
+        // GET: Passengers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Passenger passenger = repository.Find(id);
+            if (passenger == null)
+            {
+                return HttpNotFound();
+            }
+            return View(passenger);
+        }
+
+        // GET: Passengers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Passengers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Blocked")] Passenger passenger)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.Insert(passenger);
+                repository.Save();
+                return RedirectToAction("Index");
+            }
+
+            return View(passenger);
+        }
+
+        // GET: Passengers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Passenger passenger = repository.Find(id);
+            if (passenger == null)
+            {
+                return HttpNotFound();
+            }
+            return View(passenger);
+        }
+
+        // POST: Passengers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Blocked")] Passenger passenger)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.Update(passenger);
+                repository.Save();
+                return RedirectToAction("Index");
+            }
+            return View(passenger);
+        }
+
+        // GET: Passengers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Passenger passenger = repository.Find(id);
+            if (passenger == null)
+            {
+                return HttpNotFound();
+            }
+            return View(passenger);
+        }
+
+        // POST: Passengers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Passenger passenger = repository.Find(id);
+            repository.Delete(id);
+            repository.Save();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                repository.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
